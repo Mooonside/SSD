fixed gradient nan

datasets/pascal_voc_reader.py
@@ -48,12 +48,13 @@ def decode(serialized_example):
 def extract(features):
     name = features['image/name']
     image = features['image/encoded']
+    # read data in the format xmins, ymins, xmaxs, ymaxes as writer writes
     xmins = tf.sparse_tensor_to_dense(features['image/object/bbox/xmin'])
     ymins = tf.sparse_tensor_to_dense(features['image/object/bbox/ymin'])
     xmaxs = tf.sparse_tensor_to_dense(features['image/object/bbox/xmax'])
     ymaxs = tf.sparse_tensor_to_dense(features['image/object/bbox/ymax'])
     labels = tf.sparse_tensor_to_dense(features['image/object/bbox/label'])
-    # read data in the format xmins, ymins, xmaxs, ymaxes !
+    # stack use standard order
     bboxes = tf.transpose(tf.stack([ymins, xmins, ymaxs, xmaxs]))
 
     return name, image, labels, bboxes

ssd.py
@@ -1,13 +1,22 @@
+import matplotlib.image as mpimg
 import numpy as np
 import tensorflow as tf
 
 from backbones.vgg_16 import vgg_conv_block
-from datasets.pascal_voc_reader import get_dataset, get_next_batch, TRAIN_DIR
-from tf_ops.wrap_ops import max_pool2d, conv2d, batch_norm2d, tensor_shape, drop_out,\
+from tf_ops.wrap_ops import max_pool2d, conv2d, batch_norm2d, tensor_shape, drop_out, \
     softmax_with_logits, smooth_l1, l2_norm_1D, LOSS_COLLECTIONS
 
 arg_scope = tf.contrib.framework.arg_scope
-DEBUG_COLLECTIONS = 'debug_collections'
+
+DEBUG_SCOPE = 'debug'
+
+
+class DEBUG:
+    def __init__(self):
+        pass
+
+
+DEBUG_VARS = DEBUG()
 
 
 class SSDParameters(object):
@@ -28,7 +37,7 @@ class SSDParameters(object):
                 [1, 2, .5]
             ]
         self.anchor_prior_scaling = [0.1, 0.1, 0.2, 0.2]
-        self.f2b_l2_norm = [True, False, False, False, False, False, False]
+        self.f2b_l2_norm = [True, False, False, False, False, False]
 
 
 default_params = SSDParameters()
@@ -149,8 +158,8 @@ def _layer_anchors(feature_shape, feature_step, scale_c, scale_n, ratios):
     # support broadcasting in encoding part
     y = np.expand_dims(y, axis=-1)
     x = np.expand_dims(x, axis=-1)
-    # relative position in feature map is the aligned relative position in whole image
-    y = (y + 0.5) * feature_step  / default_params.img_shape[0]
+    # may have (kernel - 1) / 2 pixels misalignment
+    y = (y + 0.5) * feature_step / default_params.img_shape[0]
     x = (x + 0.5) * feature_step / default_params.img_shape[1]
 
     h = []
@@ -259,10 +268,10 @@ def _layer_encoding(layer_anchors, labels, bboxes, background_label=0):
     encode_w = encode_xmax - encode_xmin
     # Do Bbox regression here
     # [h , w, c]  = ([h , w, c] - [h , w, 1]) / [c]
-    encode_cy = (encode_cy - anchors_cy) / anchors_h
-    encode_cx = (encode_cx - anchors_cx) / anchors_w
-    encode_h = tf.log(encode_h / anchors_h)
-    encode_w = tf.log(encode_w / anchors_w)
+    encode_cy = (encode_cy - anchors_cy) / anchors_h / default_params.anchor_prior_scaling[0]
+    encode_cx = (encode_cx - anchors_cx) / anchors_w / default_params.anchor_prior_scaling[1]
+    encode_h = tf.log(encode_h / anchors_h) / default_params.anchor_prior_scaling[2]
+    encode_w = tf.log(encode_w / anchors_w) / default_params.anchor_prior_scaling[3]
     # use SSD official orders instead !!!
     encode_locations = tf.stack([encode_cx, encode_cy, encode_w, encode_h], axis=-1)
     return encode_locations, encode_labels, encode_ious
@@ -302,9 +311,12 @@ def _layer_decode(locations, layer_anchors, clip=True):
     return bboxes
 
 
-def _layer_loss(locations, scores, encode_locations, encode_labels, encode_ious, pos_th, neg_ratio):
+def _layer_loss(locations, scores, encode_locations, encode_labels, encode_ious, pos_th, neg_th, neg_ratio,
+                background_label=0):
     """
-    Calculate loss for one layer
+    Calculate loss for one layer,
+    encode_labels corresponds to the GT box with highest iou, but this iou can be less than neg_th!
+    so need to process and create new labels !
     :param locations: predicted locations [1, H, W, K, 4 ]
     :param scores: predicted scores [1, H, W, K, 21]
     :param encode_locations: [H, W, K, 4]
@@ -314,21 +326,39 @@ def _layer_loss(locations, scores, encode_locations, encode_labels, encode_ious,
     """
     positive_mask = encode_ious > pos_th
     positive_num = tf.reduce_sum(tf.cast(positive_mask, tf.int32))
-    # if no positive , ensure still some negatives, e.g. at least four bboxes along H side
-    negative_num = tf.maximum(neg_ratio * positive_num, tf.shape(encode_locations)[0] * 4)
-    # ensure it is less than the number of all available bboxes
-    negative_num = tf.minimum(negative_num, tf.reduce_prod(encode_ious.shape))
-    # Hard Negative Mining
-    neg_values, _ = tf.nn.top_k(tf.reshape(-1.0 * encode_ious, [-1]), k=negative_num)
-    # in case that -neg_value[-1] is larger than pos_th
+
+    # need to redefine the labels, those assgined to some class with iou < neg_th, should be assgined to background
     negative_mask = tf.logical_and(
-        encode_ious < -neg_values[-1],
+        encode_ious < neg_th,
         tf.logical_not(positive_mask)
+    )
+    negative_labels = tf.where(negative_mask,
+                               background_label * tf.cast(negative_mask, tf.int32),
+                               encode_labels)
+
+    # calculate background scores
+    neg_scores = tf.nn.softmax(scores, axis=-1)[:, :, :, background_label]
+    neg_scores = tf.where(negative_mask,
+                          neg_scores,
+                          # set positive ones's negative score to be 1, so that it won't be count in top_k
+                          1.0 - tf.cast(negative_mask, tf.float32)
+                          )
+    # solve # negative, add one so that neg_values has more than one value
+    max_negative_num = 1 + tf.reduce_sum(tf.cast(negative_mask, tf.int32))
+    negative_num = tf.maximum(neg_ratio * positive_num, tf.shape(encode_locations)[0] * 4)
+    negative_num = tf.minimum(negative_num, max_negative_num)
+
+    # Hard Negative Mining :
+    # find those with lower background scores, but are indeed background!
+    neg_values, _ = tf.nn.top_k(tf.reshape(-1.0 * neg_scores, [-1]), k=negative_num)
+    negative_mask = tf.logical_and(
+        negative_mask,
+        neg_scores < -neg_values[-1]
     )
 
     positive_mask = tf.cast(positive_mask, tf.float32)
     negative_mask = tf.cast(negative_mask, tf.float32)
-    tf.add_to_collection(DEBUG_COLLECTIONS, -neg_values[-1])
+
     with tf.name_scope('cross_entropy_loss'):
         with tf.name_scope('positive'):
             pos_loss = softmax_with_logits(predictions=scores,
@@ -338,24 +368,24 @@ def _layer_loss(locations, scores, encode_locations, encode_labels, encode_ious,
                                            loss_collections=LOSS_COLLECTIONS)
         with tf.name_scope('negative'):
             neg_loss = softmax_with_logits(predictions=scores,
-                                           labels=encode_labels,
+                                           labels=negative_labels,
                                            ignore_labels=[],
                                            weights=tf.reshape(negative_mask, [-1]),
                                            loss_collections=LOSS_COLLECTIONS
                                            )
 
     with tf.name_scope('bbox_regression_loss'):
-        reg_loss = smooth_l1(locations - encode_locations)
-        reg_loss = tf.reduce_sum(reg_loss, axis=-1)
+        bbox_loss = smooth_l1(locations - encode_locations)
+        bbox_loss = tf.reduce_sum(bbox_loss, axis=-1)
 
         # [H*W*K]
-        reg_loss = tf.reduce_mean(
-            reg_loss * positive_mask,
-            name='regression_loss'
+        bbox_loss = tf.reduce_mean(
+            bbox_loss * positive_mask,
+            name='bbox_regression_loss'
         )
-        tf.add_to_collection(LOSS_COLLECTIONS, reg_loss)
+        tf.add_to_collection(LOSS_COLLECTIONS, bbox_loss)
 
-    return pos_loss, neg_loss, reg_loss
+    return pos_loss, neg_loss, bbox_loss
 
 
 def layers_predictions(end_points):
@@ -421,25 +451,26 @@ def layers_decoding(gather_locations, gather_anchors, clip=True):
     return gather_decode_bboxes
 
 
-def layers_loss(prediction_gathers, encoding_gathers, pos_th=0.5, neg_ratio=3):
+def layers_loss(prediction_gathers, encoding_gathers, pos_th=0.5, neg_th=0.3, neg_ratio=3):
     gather_pred_locations, gather_pred_scores = prediction_gathers
     gather_truth_locations, gather_truth_labels, gather_truth_ious = encoding_gathers
 
-    gather_pos_loss, gather_neg_loss, gather_reg_loss = [], [], []
+    gather_pos_loss, gather_neg_loss, gather_bbox_loss = [], [], []
     for idx in range(len(default_params.feat_layers)):
-        pos_loss, neg_loss, reg_loss = _layer_loss(
+        pos_loss, neg_loss, bbox_loss = _layer_loss(
             locations=gather_pred_locations[idx],
             scores=gather_pred_scores[idx],
             encode_locations=gather_truth_locations[idx],
             encode_labels=gather_truth_labels[idx],
             encode_ious=gather_truth_ious[idx],
             pos_th=pos_th,
+            neg_th=neg_th,
             neg_ratio=neg_ratio
         )
         gather_pos_loss.append(pos_loss)
         gather_neg_loss.append(neg_loss)
-        gather_reg_loss.append(reg_loss)
-    return gather_pos_loss, gather_neg_loss, gather_reg_loss
+        gather_bbox_loss.append(bbox_loss)
+    return gather_pos_loss, gather_neg_loss, gather_bbox_loss
 
 
 def layers_select_nms(gather_pred_scores, gather_decode_bboxes, select_th=0.5, nms_th=0.45, nms_k=200, num_classes=21):
@@ -482,7 +513,6 @@ def layers_select_nms(gather_pred_scores, gather_decode_bboxes, select_th=0.5, n
         select_score = tf.gather(nms_score, select_idxes)
         select_boxes = tf.gather(nms_boxes, select_idxes)
 
-
         gather_scores[class_id] = select_score
         gather_bboxes[class_id] = select_boxes
 
@@ -490,28 +520,84 @@ def layers_select_nms(gather_pred_scores, gather_decode_bboxes, select_th=0.5, n
 
 
 if __name__ == '__main__':
-    dataset = get_dataset(dir=TRAIN_DIR, batch_size=1, num_epochs=1)
-    name, image, labels, bboxes = get_next_batch(dataset)
+    # dataset = get_dataset(dir=TRAIN_DIR, batch_size=1, num_epochs=1)
+    # name, image, labels, bboxes = get_next_batch(dataset)
 
-    # name_v, image_v, labels_v, bboxes_v = sess.run([name, image, labels, bboxes])
-    # print(name_v)
-    # print(bboxes_v)
+    # Test with one image
+    raw_image = tf.placeholder(tf.uint8, shape=(None, None, 3))
+    labels = tf.placeholder(tf.int32, shape=(None, None))
+    bboxes = tf.placeholder(tf.float32, shape=(None, None, 4))
 
-    # inputs = tf.placeholder(shape=[None, 300, 300, 3], dtype=tf.float32, name='inputs')
-    with arg_scope(ssd_arg_scope()):
-        net, end_points, prediction_gathers = ssd_vgg16(image, scope='ssd_vgg16_300')
-        gather_locations, gather_scores = prediction_gathers
+    out_shape = (300, 300)
+    image = tf.to_float(raw_image)
+    image = image - tf.constant([123., 117., 104.])
+    image = tf.image.resize_images(image, (300, 300), method=tf.image.ResizeMethod.BILINEAR, align_corners=False)
+    image = tf.expand_dims(image, axis=0)
+    with tf.device('/GPU:0'):
+        with arg_scope(ssd_arg_scope(is_training=False)):
+            net, end_points, prediction_gathers = ssd_vgg16(image, scope='ssd_vgg16_300')
+            gather_locations, gather_scores = prediction_gathers
 
-    gather_anchors = layers_anchors(end_points)
-    gather_decode_bboxes = layers_decoding(gather_locations, gather_anchors)
-    gather_scores, gather_bboxes = layers_select_nms(gather_scores, gather_decode_bboxes)
-    # # For training
-    # encoding_gathers = layers_encoding(all_anchors, labels, bboxes)
-    # gather_pos_loss, gather_neg_loss, gather_reg_loss = layers_loss(prediction_gathers, encoding_gathers)
-    #
-    sess = tf.Session()
+        # # For inference
+        # gather_anchors = layers_anchors(end_points)
+        # gather_decode_bboxes = layers_decoding(gather_locations, gather_anchors)
+        # gather_scores, gather_bboxes = layers_select_nms(gather_scores, gather_decode_bboxes)
+
+        # For training
+        gather_anchors = layers_anchors(end_points)
+        encoding_gathers = layers_encoding(gather_anchors, labels, bboxes)
+        gather_locations, gather_labels, gather_ious = encoding_gathers
+        gather_pos_loss, gather_neg_loss, gather_bbox_loss = layers_loss(prediction_gathers, encoding_gathers)
+        pos_loss = tf.reduce_sum(gather_pos_loss)
+        neg_loss = tf.reduce_sum(gather_neg_loss)
+        box_loss = tf.reduce_sum(gather_bbox_loss)
+
+        total_loss = pos_loss + neg_loss + box_loss
+
+        variables = tf.global_variables()
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=1)
+        grads_vars = optimizer.compute_gradients(total_loss, variables)
+        grads_vars = [(grad, var) for grad, var in grads_vars if grad is not None]
+
+    # load example 2007_000027.jpg
+    image_v = mpimg.imread('2007_000027.jpg')
+    bboxes_v = np.asarray(
+        [
+            [[101, 174, 351, 349]]
+        ]
+    ).reshape([1, -1, 4])
+
+    image_h, image_w, _ = image_v.shape
+    bboxes_v = bboxes_v / np.asarray([image_h, image_w, image_h, image_w])
+    labels_v = np.asarray(
+        [
+            [15]
+        ]
+    ).reshape([1, -1])
+    config = tf.ConfigProto(log_device_placement=False, allow_soft_placement=True)
+    sess = tf.Session(config=config)
     sess.run(tf.global_variables_initializer())
-    sess.run([gather_locations])
+    saver = tf.train.Saver()
+    saver.restore(sess, '/home/yifeng/TF_Models/ckpts/ssd_vgg16_300/SSD_VGG300_120000.ckpt')
+    # saver.restore(sess, '/mnt/disk/chenyifeng/TF_Models/ptrain/ssd_vgg16_300/SSD_VGG300_120000.ckpt')
+    # saver.save(sess, save_path='/mnt/disk/chenyifeng/TF_Models/ptrain/ssd_vgg16_300_17/SSD_VGG300_120000.ckpt')
 
-    # For Evaling
-    # print(labels_v)
+    # p_v, n_v, r_l = sess.run([gather_pos_loss, gather_neg_loss, gather_bbox_loss], feed_dict={
+    #     bboxes: bboxes_v,
+    #     labels: labels_v,
+    #     raw_image: image_v
+    # })
+    # print(p_v, n_v, r_l)
+    tl, xxx = sess.run([total_loss, grads_vars], feed_dict={
+        bboxes: bboxes_v,
+        labels: labels_v,
+        raw_image: image_v
+    })
+    print(tl)
+    # print(xxx)
+    for idx, value in enumerate(xxx):
+        print(grads_vars[idx][1].name, np.any(np.isnan(np.asarray(value[0]))))
+        if not np.any(np.isnan(np.asarray(value[0]))):
+            print(np.mean(value[0]))
+    print('=' * 8)
+    # print(p_v, n_v, r_l))

train.py
@@ -2,9 +2,10 @@ import os
 
 from datasets.pascal_voc_reader import get_dataset, get_next_batch, TRAIN_DIR
 from datasets.pascal_voc_utils import pascal_voc_classes
-from ssd import ssd_arg_scope, ssd_vgg16, default_params, layers_anchors, layers_loss, layers_encoding
+from ssd import ssd_arg_scope, ssd_vgg16, default_params, layers_anchors, \
+    layers_loss, layers_encoding, layers_decoding, layers_select_nms
 from tf_ops.benchmarks import mAP, mIOU
-from tf_ops.visualize import paint, compare
+from tf_ops.visualize import draw_bbox
 from tf_ops.wrap_ops import *
 from tf_utils import partial_restore, add_gradient_summary, \
     add_var_summary, add_activation_summary, parse_device_name, add_iou_summary
@@ -35,7 +36,7 @@ tf.app.flags.DEFINE_float('gpu_fraction', 0.8, 'gpu memory fraction')
 tf.app.flags.DEFINE_boolean('allow_growth', True, 'allow memory growth')
 
 # regularization
-tf.app.flags.DEFINE_float('weight_reg_scale', 1e-5, 'weight regularization scale')
+tf.app.flags.DEFINE_float('weight_reg_scale', 4e-5, 'weight regularization scale')
 tf.app.flags.DEFINE_string('weight_reg_func', 'l2', 'use which func to regularize weight')
 tf.app.flags.DEFINE_float('bias_reg_scale', None, 'bias regularization scale')
 tf.app.flags.DEFINE_string('bias_reg_func', None, 'use which func to regularize bias')
@@ -44,10 +45,10 @@ tf.app.flags.DEFINE_string('bias_reg_func', None, 'use which func to regularize 
 tf.app.flags.DEFINE_string('summaries_dir', '/home/chenyifeng/TF_Logs/SSD/vgg16_300/sgpu',
                            'where to store summary log')
 
-tf.app.flags.DEFINE_string('pretrained_ckpts', '/home/chenyifeng/TF_Models/ptrain/ssd_vgg16_300/SSD_VGG300_120000.ckpt',
+tf.app.flags.DEFINE_string('pretrained_ckpts', '/mnt/disk/chenyifeng/TF_Models/ptrain/ssd_vgg16_300/SSD_VGG300_120000.ckpt',
                            'where to load pretrained model')
 
-tf.app.flags.DEFINE_string('last_ckpt', '/home/chenyifeng/TF_Models/atrain/SSD/vgg16_300/sgpu',
+tf.app.flags.DEFINE_string('last_ckpt', None,
                            'where to load last saved model')
 
 tf.app.flags.DEFINE_string('next_ckpt', '/home/chenyifeng/TF_Models/atrain/SSD/vgg16_300/sgpu',
@@ -83,9 +84,10 @@ with tf.device(store_device):
     global_step = tf.Variable(0, trainable=False, name='global_step', dtype=tf.int64)
     # read data
     default_params.img_shape = [FLAGS.reshape_height, FLAGS.reshape_weight]
-    name_batch, image_batch, labels_batch, bboxes_batch  = get_next_batch(get_dataset(
+    name_batch, image_batch, labels_batch, bboxes_batch = get_next_batch(get_dataset(
         dir=FLAGS.data_dir, batch_size=FLAGS.batch_size, num_epochs=FLAGS.epoch_num, reshape_size=default_params.img_shape)
     )
+    print(image_batch, labels_batch, bboxes_batch)
 
 # inference
 with arg_scope([get_variable], device=store_device):
@@ -94,14 +96,26 @@ with arg_scope([get_variable], device=store_device):
                   bias_init=tf.zeros_initializer, bias_reg=bias_reg, is_training=True)):
             net, endpoints, prediction_gathers = ssd_vgg16(image_batch, scope='ssd_vgg16_300')
 
-        all_anchors = layers_anchors(endpoints)
-        encoding_gathers = layers_encoding(all_anchors, labels_batch, bboxes_batch)
-        gather_locations, gather_labels, gather_ious = encoding_gathers
+        # predictions of bboxes
+        gather_pred_locations, gather_scores = prediction_gathers
+
+        # encode ground truth for bboxes
+        gather_anchors = layers_anchors(endpoints)
+        encoding_gathers = layers_encoding(gather_anchors, labels_batch, bboxes_batch)
+        # gather_gt_locations, gather_labels, gather_ious = encoding_gathers
+
+        # solve for loss
         gather_pos_loss, gather_neg_loss, gather_box_loss = layers_loss(prediction_gathers, encoding_gathers)
-        # to do : ? check other strategies!
-        pos_loss = tf.reduce_mean(gather_pos_loss)
-        neg_loss = tf.reduce_mean(gather_neg_loss)
-        box_loss = tf.reduce_mean(gather_box_loss)
+
+        # for visualization
+        gather_decode_bboxes = layers_decoding(gather_pred_locations, gather_anchors, clip=True)
+        scores, bboxes = layers_select_nms(gather_scores, gather_decode_bboxes, select_th=0.5)
+        image_batch = draw_bbox(image_batch, scores, bboxes)
+
+        # TODO : ? check other strategies!
+        pos_loss = tf.reduce_sum(gather_pos_loss)
+        neg_loss = tf.reduce_sum(gather_neg_loss)
+        box_loss = tf.reduce_sum(gather_box_loss)
 
         reg_loss = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
         reg_loss = tf.add_n(reg_loss)
@@ -112,7 +126,8 @@ with arg_scope([get_variable], device=store_device):
         decay_learning_rate = tf.train.exponential_decay(FLAGS.weight_learning_rate, global_step,
                                                          decay_steps=10000, decay_rate=0.96, staircase=True)
 
-        optimizer = tf.train.MomentumOptimizer(learning_rate=decay_learning_rate, momentum=FLAGS.momentum)
+        # optimizer = tf.train.MomentumOptimizer(learning_rate=decay_learning_rate, momentum=FLAGS.momentum)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=decay_learning_rate)
         ratio = (FLAGS.weight_learning_rate / FLAGS.bias_learning_rate) \
             if FLAGS.bias_learning_rate is not None else 2
 
@@ -120,51 +135,54 @@ with arg_scope([get_variable], device=store_device):
         weight_vars = tf.get_collection(weight_collections)
         bias_vars = tf.get_collection(bias_collections)
 
+        # weight_grads = optimizer.compute_gradients(total_loss, weight_vars)
+        # weight_grads = [(tf.clip_by_norm(grad, clip_norm=FLAGS.clip_grad_by_norm), var)
+        #                 for grad, var in weight_grads if grad is not None]
+        #
+        # bias_grads = optimizer.compute_gradients(total_loss, bias_vars)
+        # bias_grads = [(tf.clip_by_norm(ratio * grad, clip_norm=FLAGS.clip_grad_by_norm), var)
+        #               for grad, var in bias_grads if grad is not None]
         weight_grads = optimizer.compute_gradients(total_loss, weight_vars)
-        weight_grads = [(tf.clip_by_norm(grad, clip_norm=FLAGS.clip_grad_by_norm), var)
+        weight_grads = [(grad, var)
                         for grad, var in weight_grads if grad is not None]
 
         bias_grads = optimizer.compute_gradients(total_loss, bias_vars)
-        bias_grads = [(tf.clip_by_norm(ratio * grad, clip_norm=FLAGS.clip_grad_by_norm), var)
+        bias_grads = [(grad, var)
                       for grad, var in bias_grads if grad is not None]
-
         # set up train operation
         train_op = optimizer.apply_gradients(weight_grads + bias_grads, global_step=global_step)
 
     # add summaries
-    with tf.name_scope('summary_input_output'):
-        tf.summary.image('image_batch', image_batch, max_outputs=1)
-        tf.summary.scalar('pos_loss', pos_loss)
-        tf.summary.scalar('neg_loss', neg_loss)
-        tf.summary.scalar('box_loss', box_loss)
-        tf.summary.scalar('reg_loss', reg_loss)
-        tf.summary.scalar('total_loss', total_loss)
-        # tf.summary.scalar('learning_rate', decay_learning_rate)
-
-
-    with tf.name_scope('summary_vars'):
-        for weight in weight_vars:
-            add_var_summary(weight)
-        for bias in bias_vars:
-            add_var_summary(bias)
-
-    with tf.name_scope('summary_grads'):
-        for grad, var in weight_grads:
-            add_gradient_summary(grad, var)
-        for grad, var in bias_grads:
-            add_gradient_summary(grad, var)
-
-    with tf.name_scope('summary_activations'):
-        for activations in endpoints.keys():
-            add_activation_summary(endpoints[activations])
+    # with tf.name_scope('summary_input_output'):
+    #     tf.summary.image('image_batch', image_batch, max_outputs=1)
+    #     tf.summary.scalar('pos_loss', pos_loss)
+    #     tf.summary.scalar('neg_loss', neg_loss)
+    #     tf.summary.scalar('box_loss', box_loss)
+    #     tf.summary.scalar('reg_loss', reg_loss)
+    #     tf.summary.scalar('total_loss', total_loss)
+    #     # tf.summary.scalar('learning_rate', decay_learning_rate)
+    #
+    # with tf.name_scope('summary_vars'):
+    #     for weight in weight_vars:
+    #         add_var_summary(weight)
+    #     for bias in bias_vars:
+    #         add_var_summary(bias)
+    #
+    # with tf.name_scope('summary_grads'):
+    #     for grad, var in weight_grads:
+    #         add_gradient_summary(grad, var)
+    #     for grad, var in bias_grads:
+    #         add_gradient_summary(grad, var)
+    #
+    # with tf.name_scope('summary_activations'):
+    #     for activations in endpoints.keys():
+    #         add_activation_summary(endpoints[activations])
 
 merge_summary = tf.summary.merge_all()
 train_writer = tf.summary.FileWriter(FLAGS.summaries_dir, sess.graph)
 saver = tf.train.Saver(max_to_keep=3)
 
-sess.run(tf.global_variables_initializer())
-
-# initialize
+# # initialize
 ckpt = None
 if FLAGS.last_ckpt is not None:
     ckpt = tf.train.latest_checkpoint(FLAGS.last_ckpt)
@@ -174,30 +192,39 @@ if FLAGS.last_ckpt is not None:
         print('Recovering From {}'.format(ckpt))
     else:
         print('No previous Model Found in {}'.format(ckpt))
-        if FLAGS.pretrained_ckpts is not None:
-            # pre-train priority higher
-            partial_restore_op = partial_restore(sess, tf.global_variables(), FLAGS.pretrained_ckpts)
-            sess.run(partial_restore_op)
-            print('Recovering From Pretrained Model {}'.format(FLAGS.pretrained_ckpts))
-
+elif FLAGS.pretrained_ckpts is not None:
+    # pre-train priority higher
+    sess.run(tf.global_variables_initializer())
+    partial_restore_op = partial_restore(sess, tf.global_variables(), FLAGS.pretrained_ckpts)
+    sess.run(partial_restore_op)
+    print('Recovering From Pretrained Model {}'.format(FLAGS.pretrained_ckpts))
+else:
+    sess.run(tf.global_variables_initializer())
 
 try:
     # start training
     local_step = 0
     sess.run(tf.local_variables_initializer())
-    while True:  # train until OutOfRangeError
-        pos_loss_v, neg_loss_v, box_loss_v, reg_loss_v, total_loss_v, step, summary, _ = \
-            sess.run([pos_loss, neg_loss, box_loss, reg_loss, total_loss, global_step, merge_summary, train_op])
-        train_writer.add_summary(summary, step)
-        local_step += 1
 
-        # save model per xxx steps
-        if local_step % FLAGS.save_per_step == 0 and local_step > 0:
-            save_path = saver.save(sess, os.path.join(FLAGS.next_ckpt,
-                                                      '{:.3f}_{}.ckpt'.format(total_loss_v, step)))
-            print("Model saved in path: %s" % save_path)
-        print("Step {} : Total Loss {:.3f}  Pos Loss {:.3f}  Neg loss {:.3f} Box Loss {:.3f} REG Loss {:.3f}"
-              .format(step, total_loss_v, pos_loss_v, neg_loss_v, box_loss_v, reg_loss_v))
+    for i in range(3):  # train until OutOfRangeError
+        # pos_loss_v, neg_loss_v, box_loss_v, reg_loss_v, total_loss_v, step, summary = \
+        #     sess.run([pos_loss, neg_loss, box_loss, reg_loss, total_loss, global_step, merge_summary])
+        # train_writer.add_summary(summary, step)
+
+        xxx  = sess.run(weight_grads)
+        print(xxx[0][0])
+        # for idx, value in enumerate(xxx):
+        #     print(weight_grads[idx][1].name, np.any(np.isnan(np.asarray(value[0]))))
+        #     if  not np.any(np.isnan(np.asarray(value[0]))):
+        #         print(np.mean(value[0]))
+        # print('='*8)
+        #
+        # if local_step % FLAGS.save_per_step == 0 and local_step > 0:
+        #     save_path = saver.save(sess, os.path.join(FLAGS.next_ckpt,
+        #                                               '{:.3f}_{}.ckpt'.format(total_loss_v, step)))
+        #     print("Model saved in path: %s" % save_path)
+        # print("Step {} : Total Loss {:.3f}  Pos Loss {:.3f}  Neg loss {:.3f} Box Loss {:.3f} REG Loss {:.3f}"
+        #       .format(step, total_loss_v, pos_loss_v, neg_loss_v, box_loss_v, reg_loss_v))
 
 except tf.errors.OutOfRangeError:
     print('Done training')

utils/visualization.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
-from numpy import random
+import numpy as np
 
 
-def plt_bboxes(img, scores, bboxes, figsize=(10, 10), palette=None, linewidth=1.5, ignore_labels=[0]):
+
+def plt_bboxes(img, scores, bboxes, figsize=(10, 10), name_map=None, palette=None, linewidth=1.5, ignore_labels=[0]):
     """Visualize bounding boxes. Largely inspired by SSD-MXNET!
     """
     fig = plt.figure(figsize=figsize)
@@ -16,13 +17,13 @@ def plt_bboxes(img, scores, bboxes, figsize=(10, 10), palette=None, linewidth=1.
             continue
         else:
             try:
-                cls_score = scores[cls_id][0]
-                cls_bboxes = bboxes[cls_id][0]
+                cls_score = np.reshape(scores[cls_id], [-1])
+                cls_bboxes = np.reshape(bboxes[cls_id], [-1, 4])
             except:
                 continue
 
             if palette is None:
-                cls_color = (random.random(), random.random(), random.random())
+                cls_color = (np.random.random(), np.random.random(), np.random.random())
             else:
                 cls_color = tuple(palette[cls_id, :] / 255.0)
 
@@ -31,14 +32,17 @@ def plt_bboxes(img, scores, bboxes, figsize=(10, 10), palette=None, linewidth=1.
                 xmin = int(cls_bboxes[bbox_idx][1] * width)
                 ymax = int(cls_bboxes[bbox_idx][2] * height)
                 xmax = int(cls_bboxes[bbox_idx][3] * width)
-                #                 print("Class:{}, Score:{:.3f}, Bboxes:{}" .format(cls_id, bbox_score, cls_bboxes[bbox_idx]))
+                # print("Class:{}, Score:{:.3f}, Bboxes:{}" .format(cls_id, bbox_score, cls_bboxes[bbox_idx]))
 
                 rect = plt.Rectangle((xmin, ymin), xmax - xmin,
                                      ymax - ymin, fill=False,
                                      edgecolor=cls_color,
                                      linewidth=linewidth)
                 plt.gca().add_patch(rect)
-                class_name = str(cls_id)
+                if name_map is None:
+                    class_name = str(cls_id)
+                else:
+                    class_name = name_map[cls_id]
                 plt.gca().text(xmin, ymin - 2,
                                '{:s} | {:.3f}'.format(class_name, bbox_score),
                                bbox=dict(facecolor=cls_color, alpha=0.5),